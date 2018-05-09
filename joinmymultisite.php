45a795cf4b07a19a8df39477f9e4c8a49d706ad7
Mika Epstein (Ipstenu) <ipstenu@halfelf.org>
2018-05-09 12:48:58 -0700

    Updating documentation for GDPR.

--- joinmymultisite.php
+++ joinmymultisite.php
@@ -11,24 +11,24 @@ Text Domain: join-my-multisite
 
 	Copyright 2012-2017 Mika Epstein (email: ipstenu@halfelf.org)
 
-    This file is part of Join My Multisite, a plugin for WordPress.
+	This file is part of Join My Multisite, a plugin for WordPress.
 
-    Join My Multisite is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 2 of the License, or
-    (at your option) any later version.
+	Join My Multisite is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
 
-    Join My Multisite is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	Join My Multisite is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with WordPress.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with WordPress.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 if (!defined('ABSPATH')) {
-    die();
+	die();
 }
 
 $exit_msg_multisite = 'This plugin only functions on WordPress Multisite.';
@@ -51,7 +51,7 @@ class JMM {
 			
 			// Registers our widget.
 			function jmm_load_add_user_widgets() {
-			    include_once( 'lib/widget.php');
+				include_once( 'lib/widget.php');
 			}
 			
 			// This is what controls how people get added.
@@ -77,13 +77,13 @@ class JMM {
 	 *
 	 * @since 1.9
 	 */
-    public function network_admin_notices_users_can_register() {
-	    ?>
-	    <div class="notice update-nag">
-	        <p><?php printf( __('Join My Multisite requires user registration to be enabled. Please <a href="%s">enable that in your Registration Settings</a> to continue.', 'join-my-multisite' ), admin_url( '/network/settings.php' ) ); ?></p>
-	    </div>
-	    <?php
-    }
+	public function network_admin_notices_users_can_register() {
+		?>
+		<div class="notice update-nag">
+			<p><?php printf( __('Join My Multisite requires user registration to be enabled. Please <a href="%s">enable that in your Registration Settings</a> to continue.', 'join-my-multisite' ), admin_url( '/network/settings.php' ) ); ?></p>
+		</div>
+		<?php
+	}
 
 	/**
 	 * Donate Link
@@ -91,13 +91,13 @@ class JMM {
 	 * @since 1.0
 	 * @access public
 	 */
-    public static function donate_link($links, $file) {
-        if ($file == plugin_basename(__FILE__)) {
-                $donate_link = '<a href="https://ko-fi.com/A236CENl/">Donate</a>';
-                $links[] = $donate_link;
-        }
-        return $links;
-    }
+	public static function donate_link($links, $file) {
+		if ($file == plugin_basename(__FILE__)) {
+				$donate_link = '<a href="https://ko-fi.com/A236CEN/">Donate</a>';
+				$links[] = $donate_link;
+		}
+		return $links;
+	}
 
 	/**
 	 * Add Settings Page
@@ -106,10 +106,10 @@ class JMM {
 	 * @access public
 	 */
 	public static function add_settings_page() {
-        global $jmm_settings_page;
-        $jmm_settings_page = add_users_page(__('Join My Multisite Settings','join-my-multisite'), __('Join My Multisite','join-my-multisite'), 'manage_options', 'jmm', array('JMM', 'settings_page'));
-    	}
-    	
+		global $jmm_settings_page;
+		$jmm_settings_page = add_users_page(__('Join My Multisite Settings','join-my-multisite'), __('Join My Multisite','join-my-multisite'), 'manage_options', 'jmm', array('JMM', 'settings_page'));
+		}
+		
 	/**
 	 * Settings Page Content
 	 *
@@ -127,18 +127,18 @@ class JMM {
 	 * @since 1.0
 	 * @access public
 	 */
-    static function join_site( ) {
-        global $current_user, $blog_id;
-        
-        $jmm_options = get_option( 'helfjmm_options' );
-    
-        if(!is_user_logged_in())
-        return false;
-     
-        if( !is_user_member_of_blog() ) {
-            add_user_to_blog($blog_id, $current_user->ID, $jmm_options['role']);
-        }
-    }
+	static function join_site( ) {
+		global $current_user, $blog_id;
+		
+		$jmm_options = get_option( 'helfjmm_options' );
+	
+		if(!is_user_logged_in())
+		return false;
+	 
+		if( !is_user_member_of_blog() ) {
+			add_user_to_blog($blog_id, $current_user->ID, $jmm_options['role']);
+		}
+	}
 
 }
 
